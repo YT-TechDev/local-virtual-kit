7c0bb127682387c8b8c38946cb488805f8cd43c0
YT-TechDev <msc_prop@proton.me>
2026-06-06 11:58:33 +0900

    feat: add OBS preview mode

--- apps/web-preview/src/App.tsx
+++ apps/web-preview/src/App.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react'
 import './App.css'
 import { AvatarPreview } from './components/AvatarPreview'
 import { getPreviewModeFromSearch } from './preview/previewMode'
 
 function App() {
   const previewMode = getPreviewModeFromSearch(window.location.search)
+
+  useEffect(() => {
+    document.documentElement.dataset.previewMode = previewMode
+
+    return () => {
+      delete document.documentElement.dataset.previewMode
+    }
+  }, [previewMode])
 
   return <AvatarPreview mode={previewMode} />
 }
