754e8ec150170e11849995d6f7d25839c5fb1407
YT-TechDev <msc_prop@proton.me>
2026-06-07 10:26:04 +0900

    Restore scene lights

--- apps/web-preview/src/components/AvatarScene.tsx
+++ apps/web-preview/src/components/AvatarScene.tsx
@@ -1,5 +1,7 @@
 import{DummyAvatar as D}from'./DummyAvatar'
+import{SceneLights as L}from'./SceneLights'
 import{usePreviewTime as t}from'../hooks/usePreviewTime'
 import{usePreviewMotionFrame as f}from'../hooks/usePreviewMotionFrame'
 import{mapMotionFrameToAvatar as m}from'../motion/mapMotionFrameToAvatar'
-export function AvatarScene({source}:{source:'dummy'|'native'}){return <D motion={m(f(source,t()))}/>}
+type S='dummy'|'native'
+export function AvatarScene(p:{source:S}){return <><L/><D motion={m(f(p.source,t()))}/></>}
