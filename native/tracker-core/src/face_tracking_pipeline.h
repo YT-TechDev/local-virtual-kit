1feb6a2407a8c4a48aa2abd9bda2790322b62282
YT-TechDev <msc_prop@proton.me>
2026-06-08 21:25:24 +0900

    feat: add native face detection diagnostics

--- native/tracker-core/src/face_tracking_pipeline.h
+++ native/tracker-core/src/face_tracking_pipeline.h
@@ -3,19 +3,25 @@
 #include "face_detector.h"
 #include "tracker.h"
 
+#include <string>
+
 namespace lvk::tracker {
 
 class FaceTrackingPipeline {
  public:
   FaceTrackingPipeline(
       FaceDetector& faceDetector,
-      MotionTracker& fallbackTracker);
+      MotionTracker& fallbackTracker,
+      std::string detectorName);
 
   TrackingSample track(const PreprocessedFrame& frame);
+  const FaceDetectionDiagnostics& lastDetectionDiagnostics() const;
 
  private:
   FaceDetector& faceDetector_;
   MotionTracker& fallbackTracker_;
+  std::string detectorName_;
+  FaceDetectionDiagnostics latestDiagnostics_;
 };
 
 }  // namespace lvk::tracker
